0958b7d8a0fe7e3e2c02051d7b6e439e6720a35f
zhangyingsong <zhangyingsong@xiaomi.com>
2018-11-08 16:09:32 +0800

    init jack

---
+++ src/main/java/com/jack/jackdwson/JackDwsonApplication.java
@@ -0,0 +1,12 @@
+package com.jack.jackdwson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JackDwsonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JackDwsonApplication.class, args);
+	}
+}
