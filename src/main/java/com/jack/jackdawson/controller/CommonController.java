0431917052233fda53336015c58bed0dec82620f
zhangyingsong <zhangyingsong@xiaomi.com>
2018-11-27 15:57:18 +0800

    velocity config

--- src/main/java/com/jack/jackdawson/controller/CommonController.java
+++ src/main/java/com/jack/jackdawson/controller/CommonController.java
@@ -4,23 +4,22 @@ import com.alibaba.fastjson.JSON;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
 import java.util.List;
 
-@RestController
+@Controller
 @EnableAutoConfiguration
 public class CommonController {
 
     @Resource(name = "myDataSource1")
     private DataSource myDataSource1;
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
+    @ResponseBody
     private String index(){
         try {
             JdbcTemplate jdbcTemplate = new JdbcTemplate(myDataSource1);
@@ -35,9 +34,15 @@ public class CommonController {
         return "Hello World!";
     }
 
-    @RequestMapping(value = "/testGet", method = RequestMethod.GET)
+    @RequestMapping(value = "/testGet", method = RequestMethod.GET, produces="application/json;charset=UTF-8")
+    @ResponseBody
     public String testGet(@RequestParam long userId){
         return userId + "ok";
+    }
+
+    @RequestMapping(value = "/testVM", method = RequestMethod.GET)
+    public String testVM(){
+        return "index";
     }
 
     public static void main(String[] args) throws Exception {
